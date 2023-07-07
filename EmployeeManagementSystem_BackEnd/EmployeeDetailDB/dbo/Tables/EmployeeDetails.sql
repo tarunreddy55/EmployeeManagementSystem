6928dc3c230ba8b2590fa5c56f50176fc77279e0
Tarun2329 <ptarun@argusoft.com>
2023-07-07 11:30:13 +0530

    Added DB

---
+++ EmployeeManagementSystem_BackEnd/EmployeeDetailDB/dbo/Tables/EmployeeDetails.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[EmployeeDetails] (
+    [EmployeeId]   INT         IDENTITY (1, 1) NOT NULL,
+    [EmployeeName] NCHAR (100) NULL,
+    [EmailId]      NCHAR (100) NULL,
+    [ContactNo]    NCHAR (16)  NULL,
+    [Domain]       NCHAR (100) NULL,
+    [Experience]   NCHAR (20)  NULL,
+    [Salary]       NCHAR (20)  NULL,
+    CONSTRAINT [PK_EmployeeDetail] PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
---
+++ EmployeeManagementSystem_BackEnd/EmployeeDetailDB/dbo/Tables/LoginDetails.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[LoginDetails] (
+    [EmployeeId] INT         IDENTITY (1, 1) NOT NULL,
+    [UserName]   NCHAR (100) NULL,
+    [password]   NCHAR (16)  NULL,
+    CONSTRAINT [PK_LoginDetail] PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
